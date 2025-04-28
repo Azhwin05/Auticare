ec18d46bed53c2935552b782403b80df550590c3
Azhwin2005 <ashwinarun05@gmail.com>
2025-04-28 09:07:38 +0530

    Imporved UI (NavBar,Lottie Animation)

--- app/src/main/java/com/example/auticare/SplashActivity.java
+++ app/src/main/java/com/example/auticare/SplashActivity.java
@@ -4,16 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 4000; // 4 seconds
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        LottieAnimationView lottie = findViewById(R.id.lottieAnimationView);
+        lottie.setAnimation("welcome_animation.json"); // 🟡 Your JSON file name
+        lottie.playAnimation();
+
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, LoginActivity.class));
             finish();
-        }, 2000); // 2 seconds splash
+        }, SPLASH_TIME_OUT);
     }
 }
